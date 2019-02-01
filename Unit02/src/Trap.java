78a713faf8bd2028e17ad484ca6a922402c2bef6
Jonathan Hsu <johnnhsu1031@gmail.com>
2019-02-01 10:38:34 -0800

    added Unit02

---
+++ Unit02/src/Trap.java
@@ -0,0 +1,15 @@
+//© A+ Computer Science  -  www.apluscompsci.com
+//Name -
+//Date -
+//Class -
+//Lab  - 
+
+public class Trap
+{
+	public static double area( int base1, int base2, int height )
+	{
+		double x = base1+base2;
+		double y = x*height/2;
+		return y;
+	}
+}
