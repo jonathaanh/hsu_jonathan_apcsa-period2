64d9dadd013a053553d9879fc4b83d8e1dba58f6
Jonathan Hsu <johnnhsu1031@gmail.com>
2019-02-04 11:03:14 -0800

    Added Unit04

---
+++ Unit04/src/OddOrEven.java
@@ -0,0 +1,27 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+
+//Name -
+//Date -
+//Class - 
+//Lab  -
+
+import static java.lang.System.*;
+
+public class OddOrEven
+{
+	public static boolean isOdd( int x)
+	{
+		if(x %2 == 1) {
+			return true;
+		}
+		return false;
+	}
+	public static boolean isEven(int y )
+	{
+		if(y %2 == 0) {
+			return true;
+		}
+		return false;
+	}	
+}
