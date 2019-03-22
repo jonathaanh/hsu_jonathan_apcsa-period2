0dd2692944b625d2a30eb1c3a28de138f9d82f6e
Jonathan Hsu <johnnhsu1031@gmail.com>
2019-03-22 11:10:02 -0700

    Unit 14 partially done

---
+++ Unit14/src/MatrixCount1Runner.java
@@ -0,0 +1,18 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+//Name -
+
+public class MatrixCount1Runner
+{
+	public static void main( String args[] ) throws Exception
+	{
+		MatrixCount1 m = new MatrixCount1();
+		System.out.println("The 7 count is :: " + m.count(7));
+		System.out.println("The 8 count is :: " + m.count(8));
+		System.out.println("The 5 count is :: " + m.count(6));
+		
+	}
+}
+
+
+
